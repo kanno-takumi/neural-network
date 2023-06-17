2b066733a2a9ecdd9df9ee874170a02858b34667
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-06-17 23:50:35 +0900

    AutoEncoder実装

---
+++ AutoEncoder/train_neuralnet.py
@@ -0,0 +1,49 @@
+import numpy as np
+import sys,os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+# import keras
+#from two_layer_net import TwoLayerNet
+
+#0と1のデータ0から1000個を取得する。
+#検証用データ1000から1100
+
+#one_hot_label t_trainに関して「”true”の時one-hot表現 ”false”の時数値で扱う」
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True,one_hot_label=False) 
+t_train_0=t_train[np.where(t_train==0)] #0のデータx
+x_train_0 = x_train[np.where(t_train==0)] #0のデータt
+print("x_train_0",x_train_0)
+t_train_1 = t_train[np.where(t_train==1)] #1のデータx
+x_train_1 = x_train[np.where(t_train==1)] #1のデータt
+print("x_train_1",x_train_1)
+t_train_0to1 = np.concatenate([t_train_0, t_train_1])
+x_train_0to1 = np.concatenate([x_train_0,x_train_1])
+
+print(t_train_0to1)
+train_loss_list = []
+
+iters_num = 10 #何回勾配法使うか（イテレータ）
+train_size = x_train_0to1.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784,hidden_size=2,output_size=784)
+
+for i in range(iters_num): #1バッチに対して勾配法を用いる回数
+    print("here")
+    batch_mask = np.random.choice(train_size,batch_size)#(sizeの中からランダムな値,個数)[a,b,c,d,e,f.....]
+    x_batch = x_train[batch_mask]
+    t_batch = x_batch
+    
+    grad = network.numerical_gradient(x_batch,t_batch)
+    
+    for key in ('W1','b1','W2','b2'):
+        network.params[key] = network.params[key] - learning_rate * grad[key]
+        
+        
+    loss = network.loss(x_batch,t_batch)
+    train_loss_list.append(loss)
+
+print(train_loss_list)
+
---
+++ AutoEncoder/two_layer_net.py
@@ -0,0 +1,65 @@
+#二層のニューラルネットワークを実装 #隠れ層が1つ
+import numpy as np
+import sys,os 
+sys.path.append(os.pardir)
+from common.functions import *
+from common.gradient import numerical_gradient
+
+#params ニューラルネットワークのパラメータを保持するディクショナリ変数
+#params['W1']は1層目の重み
+#params['b1' ]は1層目のバイアス
+
+#grads 勾配を保持するディクショナリー変数 
+#numerical_gradient()の返り値
+#grads['W1']は1層目の重みの勾配
+#grads['b1']は1層目のバイアスの勾配
+
+#random.randn(入力データの個数,ニューロンの個数)
+
+class TwoLayerNet:
+    params=None
+    def __init__(self,input_size,hidden_size,output_size,weight_init_std=0.01):
+        self.params={} #ディクショナリ
+        self.params['W1'] = weight_init_std * np.random.randn(input_size,hidden_size) #平均0、分散1（標準偏差1）引数で配列の次元を返す
+        self.params['b1'] = np.zeros(hidden_size) #0で初期化した配列を作る
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+        #print(self.params)
+        
+    def predict(self,x):
+        W1,W2 = self.params['W1'],self.params['W2']
+        b1,b2 = self.params['b1'],self.params['b2']
+        #ここで再度W1,W2,b1,b2を定義して入れている
+        
+        a1 = np.dot(x,W1) + b1 #入力層からの計算
+        z1 = sigmoid(a1) #活性化関数
+        a2 = np.dot(z1,W2) + b2      
+        y = softmax(a2)    
+        #print("xのサイズ",x.shape) 
+        #print("W1のサイズ",W1.shape)
+        #print("aのサイズ",a1.shape)
+        #print("yのサイズ",y.shape)
+        return y # AIが出した結果
+    
+    def loss(self,x,t): #損失関数
+        y = self.predict(x)
+        return cross_entropy_error(y,t) #2.302944356298707のような値
+    
+    def accuracy(self,x,t): #どれくらい合っているか
+        y = self.predict(x)
+        y = np.argmax(y,axis = 1) #配列の最大を返す
+        t = np.argmax(t,axis = 1) 
+        
+        accuracy = np.sum( y==t ) / float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self,x,t): #損失関数における勾配　＃x→入力データのこと t→教師データ
+        loss_W = lambda : self.loss(x,t) #predictの結果と正解ラベルの交差エントロピー誤差  #無名関数
+        grads = {} #ディクショナリー
+        
+        #なぜ分ける必要があるのか
+        grads['W1'] = numerical_gradient(loss_W,self.params['W1']) #引数(損失関数、その関数に入れる入力)
+        grads['b1'] = numerical_gradient(loss_W,self.params['b1'])#サイズを決めるためだけにparams['b1'] ['W1'] ['W2'] ['b2']
+        grads['W2'] = numerical_gradient(loss_W,self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W,self.params['b2'])
+        return grads
