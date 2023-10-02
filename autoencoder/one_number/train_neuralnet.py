a3899d5779d19b236559d4567ee5ee9dbbb8b00c
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-10-02 22:07:52 +0900

    変更

---
+++ autoencoder/one_number/train_neuralnet.py
@@ -0,0 +1,96 @@
+import numpy as np
+#np.set_printoptions(threshold=np.inf)
+import sys,os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+from image_show import make_img
+# import keras
+#from two_layer_net import TwoLayerNet
+
+#0と1のデータ0から1000個を取得する。
+#検証用データ1000から1100
+
+def train_neuralnet():
+
+#normalize 正規化
+#one_hot_label t_trainに関して「”true”の時one-hot表現 ”false”の時数値で扱う」
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True,one_hot_label=False) 
+    #t_train_0 = t_train[np.where(t_train==0)] #0のデータt 教師データはいらない
+    x_train_0 = x_train[np.where(t_train==0)] #0のデータx　
+    x_test_0 = x_test[np.where(t_test==0)]
+    #t_test_0 = t_test[np.where(t_test==0)]　教師データはいらない
+    print("x_train_0[0]",x_train_0[0])
+    #make_img(x_train_0[0],"sample.png")
+# with open('train_out.txt', mode='w') as f:
+#     f.write(str(x_train_0[0]))
+    #t_train_1 = t_train[np.where(t_train==1)] #1のデータ
+    x_train_1 = x_train[np.where(t_train==1)] #1のデータt 
+    print("x_train_1",x_train_1)
+    
+    #t_train_0to1 = np.concatenate([t_train_0, t_train_1])
+    #x_train_0to1 = np.concatenate([x_train_0,x_train_1])#0と1のデータx
+    #print(t_train_0to1)
+    train_size = x_train_1.shape[0]
+    print("train_size",train_size) #12665
+    batch_size = 100
+    iters_num = 100 #何回勾配法使うか（イテレータ）
+    learning_rate = 0.1
+
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    #iter_per_epoch = max(train_size/batch_size,1) #1エポックあたりの繰り返し数  12665/100 （1エポック=12665）の1まとまりで何回繰り返すか。足りなければ次のエポック
+    #print("iter_per_epoch",iter_per_epoch)#126.65
+
+
+
+    network = TwoLayerNet(input_size=784,hidden_size=2,output_size=784)
+
+    for i in range(iters_num): #1バッチに対して勾配法を用いる回数
+        print("here")
+        batch_mask = np.random.choice(train_size,batch_size)#例 5207 5424 4372 133 114 4522 4704　ランダムにデータを撮ってきたいだけ
+        x_batch = x_train_1[batch_mask]
+        #print(batch_mask)
+        #print("x_batch",x_batch[0])
+        t_batch = x_train_1[batch_mask] #入出力で比べるのが同じものでないと困る
+    
+        grad = network.numerical_gradient(x_batch,t_batch)
+    
+        for key in ('W1','b1','W2','b2'):
+            network.params[key] = network.params[key] - learning_rate * grad[key]
+        
+        
+        loss = network.loss(x_batch,t_batch)
+        train_loss_list.append(loss)
+        # with open('train_out.txt', mode='a',newline='\n') as f:
+        #     f.writelines(str(loss)+",")
+    
+        # if i % iter_per_epoch == 0: #iter_per_epoch=126.65 i=126になった時だけ発動 252 
+        #     train_acc = network.accuracy(x_train_0,t_train_0)
+        #     test_acc = network.accuracy(x_test_0,t_test_0)
+        #     train_acc_list.append(train_acc)
+        #     test_acc_list.append(test_acc)
+        
+        if (i == iters_num- 1):
+            #predictの出力結果に対して、255をかければ大丈夫？？？何ならかけなくても大丈夫？
+            #line見ればわかる
+            #make_img(network.predict(x_batch[0]),"x_out.png")
+            print("x_batch[0]",x_batch[0])
+            print("t_batch[0]",t_batch[0]) #ここまでは普通にbatchデータが機能している
+            make_img(network.predict(x_batch[0]),"学習済みデータ.png") #predictした状態のものを引数で与えている
+            print("make_imgが完了しました。")
+            make_img(t_batch[0],"元のデータの例.png")
+
+    #精度を確認しているだけ
+    print("lossの出力",train_loss_list)
+    #print("accuracy",network.accuracy(t_batch[0]))
+    # print("W1",network.params['W1'])
+    # print("W2",network.params['W2'])
+    with open('W1.txt', mode='w') as f:
+        f.write(str(network.params['W1']))
+    with open('W2.txt', mode='w') as f:
+        f.write(str(network.params['W2']))
+   # print("train acc,test acc |" + str(train_acc)+","+str(test_acc))
+    
+train_neuralnet()
---
+++ autoencoder/one_number/two_layer_net.py
@@ -0,0 +1,70 @@
+#二層のニューラルネットワークを実装 #隠れ層が1つ
+import numpy as np
+import sys,os 
+sys.path.append(os.pardir+"/..")#2個前のディレクトリが知りたかったから。
+from common.functions import *
+from common.gradient import numerical_gradient
+
+#params ニューラルネットワークのパラメータを保持するディクショナリ変数
+#params['W1']は1層目の重み
+#params['b1' ]は1層目のバイアス
+
+#grads 勾配を保持するディクショナリー変数 
+#numerical_gradient()の返り値
+#grads['W1']は1層目の重みの勾配
+#grads['b1']は1層目のバイアスの勾配
+
+#random.randn(入力データの個数,ニューロンの個数)
+
+class TwoLayerNet:
+    print("os",os)
+    params=None
+    def __init__(self,input_size,hidden_size,output_size,weight_init_std=0.01):
+        self.params={} #ディクショナリ
+        self.params['W1'] = weight_init_std * np.random.randn(input_size,hidden_size) #平均0、分散1（標準偏差1）引数で配列の次元を返す
+        self.params['b1'] = np.zeros(hidden_size) #0で初期化した配列を作る
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+        #print(self.params)
+        
+    def predict(self,x):#xはバッチで入る
+        W1,W2 = self.params['W1'],self.params['W2']
+        b1,b2 = self.params['b1'],self.params['b2']
+        #ここで再度W1,W2,b1,b2を定義して入れている
+        
+        a1 = np.dot(x,W1) + b1 #入力層からの計算
+        z1 = relu(a1) #活性化関数
+        a2 = np.dot(z1,W2) + b2      
+        y = sigmoid(a2) #出力は0〜1　本来は0から255の値が入ることになっているが正規化しているため
+        
+        #print("predictの出力",y)
+        #print("xのサイズ",x.shape) 
+        #print("W1のサイズ",W1.shape)
+        #print("aのサイズ",a1.shape)
+        #print("yのサイズ",y.shape) => (100,784) = (batch,画像の数字列)
+        return y # AIが出した結果
+    
+    def loss(self,x,t): #損失関数 #引数はbatchで入る
+        y = self.predict(x)#784ニューロンとか
+        #print("yのサイズ",y.shape) (100,784)
+        return reconstruction_error(y,t) #2.302944356298707のような値　＃再構成誤差を利用
+    #reconstruction内で、batchデータをまとめて処理してlossを計算している。
+    
+    def accuracy(self,x,t): #どれくらい合っているか
+        y = self.predict(x)
+        y = np.argmax(y,axis = 1) #配列の最大を返す
+        t = np.argmax(t,axis = 1) 
+        
+        accuracy = np.sum( y==t ) / float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self,x,t): #損失関数における勾配　＃x→入力データのこと t→教師データ
+        loss_W = lambda : self.loss(x,t) #predictの結果と正解ラベルの交差エントロピー誤差  #無名関数
+        grads = {} #ディクショナリー
+        
+        #なぜ分ける必要があるのか
+        grads['W1'] = numerical_gradient(loss_W,self.params['W1']) #引数(損失関数、その関数に入れる入力)
+        grads['b1'] = numerical_gradient(loss_W,self.params['b1'])#サイズを決めるためだけにparams['b1'] ['W1'] ['W2'] ['b2']
+        grads['W2'] = numerical_gradient(loss_W,self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W,self.params['b2'])
+        return grads
